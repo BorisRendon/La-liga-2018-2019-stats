b8e874e0df3e00923d812fa68b10a755233330c3
BorisRendon <boris.rendon@ufm.edu>
2020-10-18 15:57:44 -0600

    upload data

---
+++ LaLigaStats2018_2019/ui.R
@@ -0,0 +1,20 @@
+## ui.R ##
+library(shinythemes)
+
+fluidPage(theme = shinytheme("cerulean"),
+          
+          shinythemes::themeSelector(),  # <--- Add this somewhere in the UI
+          sidebarPanel(
+              textInput("txt", "Text input:", "text here"),
+              sliderInput("slider", "Slider input:", 1, 100, 30),
+              actionButton("action", "Button"),
+              actionButton("action2", "Button2", class = "btn-primary")
+          ),
+          mainPanel(
+              tabsetPanel(
+                  tabPanel("Tab 1"),
+                  tabPanel("Tab 2")
+              )
+          )
+          
+)
