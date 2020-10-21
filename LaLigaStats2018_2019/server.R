db0c893cbf7c0e507e7412d8345af2d006ab1d1b
Maite de la Roca <delaroca.estrada21@gmail.com>
2020-10-20 18:37:41 -0600

    Players selection

--- LaLigaStats2018_2019/server.R
+++ LaLigaStats2018_2019/server.R
@@ -1,7 +1,58 @@
 
 library(shiny)
+library(dplyr)
 
-shinyServer(function(input, output) {
+jugadores <- read_delim("laliga_player_stats_spanish.csv", ";")
 
-
+shinyServer(function(input, output, session) {
+  
+  # cambiar equipo
+  observeEvent(input$players_select, {
+    newTeam <- jugadores %>% select(Equipo, Nombre) %>% filter(Nombre==input$players_select)
+    updateRadioButtons(session, 'player_team', selected=newTeam$Equipo)
+  })
+  
+  # cambiar jugadores por equipo
+  observeEvent(input$player_team, {
+    newPlayers <- jugadores %>% select(Equipo, Nombre) %>% filter(Equipo==input$player_team)
+    print(newPlayers$Nombre[1])
+    updateSelectInput(session, 'players_select',choices=newPlayers, selected=newPlayers$Nombre[1])
+  })
+  
+  # falta cambiar jugadores por posicion
+  
+  # cambiar posicion
+  observeEvent(input$players_select, {
+    newPosition <- jugadores %>% select(Posicion, Nombre) %>% filter(Nombre==input$players_select)
+    updateSelectInput(session, 'player_type', selected=newPosition$Posicion)
+  })
+  
+  output$distPie <- renderPlot({
+    # en el vector poner variable de porcentaje de partidos jugados enteros
+    pie(c(0.2,0.8), main = "Porcentaje de partidos jugados enteros")
+  })
+  
+  output$jugador <- renderText({input$players_select})
+  
+  output$dorsal <- renderText({paste0("Dorsal: ", input$players_select)})
+  
+  output$distCards <- renderPlot({
+    hist(c(1,2), col = 'darkgray', border = 'white', main="Tarjetas rojas y amarillas")
+  })
+  
+  output$team <- renderImage({
+    if (is.null(input$player_team))
+      return(NULL)
+    
+    print(input$player_team)
+    if (!is.null(input$player_team)) {
+      return(list(
+        src = paste0("www/",input$player_team, ".png"),
+        contentType = "image/png"
+      ))
+    }
+  
+    
+  }, deleteFile = FALSE)
+  
 })
