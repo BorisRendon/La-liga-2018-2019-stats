8a221301273a59699f3692ee53acbf71bca6da07
BorisRendon <boris.rendon@ufm.edu>
2020-10-21 18:41:59 -0600

    Update

--- LaLigaStats2018_2019/server.R
+++ LaLigaStats2018_2019/server.R
@@ -1,7 +1,45 @@
-
 library(shiny)
+library(ggplot2)
+library(dplyr)
+library(DT)
 
 shinyServer(function(input, output) {
 
-
+  output$tabla1 <- DT::renderDataTable({
+    DT::datatable(equipos,
+                  extensions = 'Buttons',
+                  options = list(pageLength = 10,
+                                 lengthMenu = c(5, 10, 15),
+                                 dom = 'Bfrtip',
+                                 buttons = c('csv')),
+                  filter = 'bottom'
+                  
+    ) 
+  
+  
+  })
+  
+  output$graficagoles <- renderPlot({
+     
+      ggplot(goles_por_equipo, aes(x = "", y = goles, fill = Equipo )) +
+      geom_bar(stat = "identity" , width = 1 , color= "white") +
+      coord_polar("y" ,start = 0)+ 
+      geom_text(aes(label = paste0(goles)), position = position_stack(vjust = 0.5))+
+      theme_void()
+    
+    
+  })
 })
+  
+  #output$grafica_ggplot <- renderPlot({
+   # equipos %>% 
+    #  ggplot(aes(x=HomeTeam,y=FTHG, color=color))+
+     # geom_point()+
+      #ylab("Precio")+
+      #xlab("Kilates")+
+      #ggtitle("Precio diamantes")
+
+  
+
+
+
--- LaLigaStats2018_2019/ui.R
+++ LaLigaStats2018_2019/ui.R
@@ -1,10 +1,16 @@
 ## ui.R ##
 library(shinythemes)
 library(readr)
+library(tidyverse)
+library(shiny)
+
+
 
 # equipos dataset
 equipos <- read_delim("laliga_partidos_2018-2019.csv", ";")
 nombres_equipos <- unique(equipos$AwayTeam)
+equipos %>% 
+  summarise(AwayTeam)
 # FTHG = Full Time Home Team Goals
 # FTAG = Full Time Away Team Goals
 # FTR = Full Time Result (H=Home Win, D=Draw, A=Away Win)
@@ -16,16 +22,66 @@ nombres_equipos <- unique(equipos$AwayTeam)
 jugadores <- read_delim("laliga_player_stats_spanish.csv", ";")
 nombres_jugadores <- unique(jugadores$Nombre)
 
+
+
+
+
+
+total_equipos <- equipos %>% 
+  summarise(Cantidad_equipos = n_distinct(HomeTeam))
+
+total_jugadores <- jugadores %>% 
+  summarise(jugadores_distintos = n_distinct(Nombre))
+
+total_golesaway <- equipos %>% 
+  summarise(sum(equipos$FTHG))
+
+total_goleshome <- equipos %>% 
+  summarise(sum(equipos$FTAG))
+
+total_goles <- total_golesaway + total_goleshome
+
+total_amarillas <- jugadores %>% 
+  summarise(sum(jugadores$`Tarjetas amarillas`))
+
+total_rojas <- jugadores %>% 
+  summarise(sum(jugadores$`Tarjetas rojas`))
+
+goles_por_equipo <- jugadores %>% 
+  select(Equipo,`Goles marcados`) %>% 
+  group_by(Equipo) %>% 
+  summarise(Equipo=Equipo,goles = sum(`Goles marcados`)) %>% 
+  arrange(desc(goles))
+
+goles_por_equipo <-unique(goles_por_equipo)
+  
+  
+ 
+
 fluidPage(theme = shinytheme("united"),
           mainPanel(
             tabsetPanel(
               tabPanel("Inicio",
-                       # Numero de equipos y de jugadores
+                       
+                       h5("Hay un total de ",total_equipos , "equipos en LaLiga"),
+                       h5("Hay un total de ",total_jugadores , "jugadores inscritos en LaLiga"),
+                       h5("Hay un total de ",total_goles , "goles anotados en LaLiga"),
+                       h5("Hay un total de ",total_amarillas , "tarjetas amarillas en LaLiga"),
+                       h5("Hay un total de ",total_rojas , "tarjetas rojas en LaLiga"),
+                       
+                       plotOutput('graficagoles')
+                       
+                       
+                       # Numero de equipos y de jugadores // 
                        # Top 5 equipos con mas goles
                        # Top 5 mejores jugadores > en posiciones
                        # Campeon de la temporada 2018 - 2019
-                       # Total de goles en la temporada
-                       # Total de tarjetas amarillas y rojas en la temporada
+                       # Total de goles en la temporada//
+                       # Total de tarjetas amarillas y rojas en la temporada//
+                       
+                       
+                       
+                       
                        ),
               # feature: comparar equipos y jugadores
               tabPanel("Equipos",
@@ -34,9 +90,10 @@ fluidPage(theme = shinytheme("united"),
                          selectInput("equipos_select",
                                      "Seleccionar equipo",
                                      choices=nombres_equipos)
+                         
                        ),
                        mainPanel(h2("Equipos"), 
-                                fluidRow("data"))
+                                fluidRow(fluidRow(column(6,DT::dataTableOutput('tabla1')))))
                        ),
               tabPanel("Jugadores",
                        br(),
